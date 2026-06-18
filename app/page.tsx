485048b45ed1e5415051431172c98e4e01b44805
unknown <thirunavukkarasuadaikkalam@gmail.com>
2026-06-18 13:19:36 +0530

    homepage redesign

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,30 @@ import Hero from "@/components/Hero";
 import CoreCapabilities from "@/components/CoreCapabilities";
 import Industries from "@/components/Industries";
 import GlobalPresence from "@/components/GlobalPresence";
+import ProjectLifecycle from "@/components/ProjectLifecycle";
 import LeadershipMessage from "@/components/LeadershipMessage";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
+
       <Navbar />
 
       <Hero />
 
+      <GlobalPresence />
+
       <CoreCapabilities />
 
-      <Industries />
+      <ProjectLifecycle />
 
-      <GlobalPresence />
+      <Industries />
 
       <LeadershipMessage />
 
       <Footer />
+
     </main>
   );
 }
